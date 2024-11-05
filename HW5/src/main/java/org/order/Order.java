5d48c65a6b7ea7e2c52999a0cd25b694740766e9
Andrew Lau <82020841+BalloonAnd888@users.noreply.github.com>
2024-11-05 01:34:56 -0800

    Order

--- HW5/src/main/java/org/order/Order.java
+++ HW5/src/main/java/org/order/Order.java
@@ -1,4 +1,22 @@
 package HW5.src.main.java.org.order;
 
+import HW5.src.main.java.org.food.Food;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Order {
+    private List<Food> foods = new ArrayList<>();
+
+    public void addFood(Food food) {
+        foods.add(food);
+    }
+
+    public double calculateTotalCost() {
+        double total = 0;
+        for (Food food : foods) {
+            total += food.getCost();
+        }
+        return total;
+    }
 }
--- HW5/src/test/java/OrderTest.java
+++ HW5/src/test/java/OrderTest.java
@@ -1,11 +1,22 @@
 package HW5.src.test.java;
 
+import HW5.src.main.java.org.food.Burger;
+import HW5.src.main.java.org.food.Fries;
 import HW5.src.main.java.org.order.Order;
+import HW5.src.main.java.org.toppings.Bacon;
+import HW5.src.main.java.org.toppings.Cheese;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class OrderTest {
     @Test
     public void testCalculateTotalCost() {
         Order order = new Order();
+
+        order.addFood(new Burger());
+        order.addFood(new Cheese(new Burger()));
+        order.addFood(new Bacon(new Fries()));
+        assertEquals(14.75, order.calculateTotalCost());
     }
 }
