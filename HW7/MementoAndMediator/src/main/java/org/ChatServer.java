84dee609888b5544c80a8bcfadd17db76aa3e6e3
Andrew Lau <82020841+BalloonAnd888@users.noreply.github.com>
2024-11-15 22:19:08 -0800

    init

---
+++ HW7/MementoAndMediator/src/main/java/org/ChatServer.java
@@ -0,0 +1,4 @@
+package HW7.MementoAndMediator.src.main.java.org;
+
+public class ChatServer {
+}
---
+++ HW7/MementoAndMediator/src/main/java/org/Driver.java
@@ -0,0 +1,7 @@
+package HW7.MementoAndMediator.src.main.java.org;
+
+public class Driver {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
